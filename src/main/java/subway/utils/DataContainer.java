5421805e14c402b55c57da5fe6fd57a77a913973
mainfn <_@mainfn.dev>
2023-09-19 00:36:40 +0900

    refactor: 출력 메세지 일부 분리

---
+++ src/main/java/subway/utils/DataContainer.java
@@ -0,0 +1,21 @@
+package subway.utils;
+
+public final class DataContainer {
+
+
+  public void save() {
+
+  }
+
+  private void savePreStations() {
+
+  }
+
+  private void savePreLines() {
+
+  }
+
+  private void savePreSections() {
+
+  }
+}
---
+++ src/main/java/subway/utils/FeatureSelectionMessages.java
@@ -0,0 +1,23 @@
+package subway.utils;
+
+public final class FeatureSelectionMessages {
+
+  public static final String APP_FEATURE_SELECTION_MESSAGE = "## 메인 화면\n"
+      + "1. 역 관리\n"
+      + "2. 노선 관리\n"
+      + "3. 구간 관리\n"
+      + "4. 지하철 노선도 출력\n"
+      + "Q. 종료\n"
+      + "\n"
+      + "## 원하는 기능을 선택하세요.";
+
+  public static final String STATION_FEATURE_SELECTION_MESSAGE = "## 역 관리 화면\n"
+      + "1. 역 등록\n"
+      + "2. 역 삭제\n"
+      + "3. 역 조회\n"
+      + "B. 돌아가기\n"
+      + "\n"
+      + "## 원하는 기능을 선택하세요.";
+
+
+}
