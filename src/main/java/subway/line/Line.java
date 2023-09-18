7adfd68ba6d3fb8e4ee92b74e85ab5092d39e318
mainfn <_@mainfn.dev>
2023-09-18 20:25:43 +0900

    fix: Line 및 Station name length validation 추가

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -1,16 +1,48 @@
-package subway.domain.line;
+package subway.line;
 
-public class Line {
+import subway.station.Station;
+
+public final class Line {
 
   private final String name;
+  private final Station firstStation;
+  private final Station lastStation;
 
-  public Line(String name) {
+  private Line(
+      final String name,
+      final Station firstStation,
+      final Station lastStation
+  ) {
+    validateLineNameLength(name);
     this.name = name;
+    this.firstStation = firstStation;
+    this.lastStation = lastStation;
   }
 
+  public static Line of(
+      final String name,
+      final Station firstStation,
+      final Station lastStation
+  ) {
+    return new Line(name, firstStation, lastStation);
+  }
+
+  private void validateLineNameLength(final String lineName) {
+    if (lineName.length() < 2) {
+      throw new IllegalArgumentException("노선명은 2글자 이상이어야 합니다");
+    }
+  }
+
+  // 추가 기능 구현
   public String getName() {
     return name;
   }
 
-  // 추가 기능 구현
+  public Station getFirstStation() {
+    return firstStation;
+  }
+
+  public Station getLastStation() {
+    return lastStation;
+  }
 }
--- src/main/java/subway/station/Station.java
+++ src/main/java/subway/station/Station.java
@@ -5,6 +5,7 @@ public final class Station {
   private final String name;
 
   private Station(final String name) {
+    validateStationNameLength(name);
     this.name = name;
   }
 
@@ -14,6 +15,12 @@ public final class Station {
 
   public String getName() {
     return name;
+  }
+
+  private void validateStationNameLength(final String stationName) {
+    if (stationName.length() < 2) {
+      throw new IllegalArgumentException("역명은 2글자 이상이어야 합니다.");
+    }
   }
 
   // 추가 기능 구현
